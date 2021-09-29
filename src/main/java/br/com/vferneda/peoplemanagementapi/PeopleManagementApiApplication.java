12a0390e31471072da0d358018d8b3c1e497df78
viniciusferneda <vinicius.ferneda@gmail.com>
2021-09-29 18:22:39 -0300

    First commit

---
+++ src/main/java/br/com/vferneda/peoplemanagementapi/PeopleManagementApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.vferneda.peoplemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vferneda/peoplemanagementapi/PeopleManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vferneda.peoplemanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
